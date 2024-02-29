849b3f2d05afa7b5236a777358479b233334f35c
ChancellorKupersmith <kupersmith3.6@gmail.com>
2024-02-29 09:24:01 -0800

    /bin/bash doesn't exist on nixos removing script overrides

--- config/scripts/home-rebuild.nix
+++ config/scripts/home-rebuild.nix
@@ -1,11 +1,8 @@
 { pkgs, ... }:
 
-let
-  pkgs.writeShellScriptBin = (pkgs.writeShellScriptBin.override { runTimeShell = "/bin/bash"; } );
-in
-  pkgs.writeShellScriptBin "home-rebuild" ''
-    pushd
-    cd /home/kuper/.dotfiles/KuperOS
-    home-manager switch -f home.nix
-    popd | cd
+pkgs.writeShellScriptBin "home-rebuild" ''
+  pushd
+  cd /home/kuper/.dotfiles/KuperOS
+  home-manager switch -f home.nix
+  popd | cd
 ''
--- config/scripts/os-rebuild.nix
+++ config/scripts/os-rebuild.nix
@@ -1,11 +1,8 @@
 { pkgs, ... }:
 
-let
-  pkgs.writeShellScriptBin = (pkgs.writeShellScriptBin.override { runTimeShell = "/bin/bash"; } );
-in
-  pkgs.writeShellScriptBin "os-rebuild" ''
-    pushd
-    cd /home/kuper/.dotfiles/KuperOS
-    sudo nixos-rebuild switch --flake .#nixos
-    popd | cd
+pkgs.writeShellScriptBin "os-rebuild" ''
+  pushd
+  cd /home/kuper/.dotfiles/KuperOS
+  sudo nixos-rebuild switch --flake .#nixos
+  popd | cd
 ''
